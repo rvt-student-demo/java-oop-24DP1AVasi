cc833d4f86e22b064fd4af54b1c76693914b9e14
Armands Andrejs Vasiļjevs <vasiljevs.armands@gmail.com>
2026-01-06 17:29:42 +0200

    Pasūtijumu Vēsture DONE

--- src/main/java/rvt/PasutijumuVesture.java
+++ src/main/java/rvt/PasutijumuVesture.java
@@ -5,22 +5,26 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class PasutijumuVesture {
-    try(Scanner scan = new Scanner(new File("data/data.csv"))) {
-            scan.nextLine(); // skip header
-            while(scan.hasNextLine()) {
-                String row = scan.nextLine();
-                String[] parts = row.split(",");
+    public static void main(String[] args) {
+        double sum = 0;
+        try(Scanner scan = new Scanner(new File("data/data.csv"))) {
+                scan.nextLine(); // skip header
+                while(scan.hasNextLine()) {
+                    String row = scan.nextLine();
+                    String[] parts = row.split(",");
 
-                int id = Integer.valueOf(parts[0]);
-                String name = parts[1];
-                String product = parts[2];
-                int quantity = Integer.valueOf(parts[3]);
-                double price = Double.valueOf(parts[4]);
-
-                System.out.println("Pasutijums #"+id+": "+name+" pasutija "+quantity+" x "+product+" ("+price+" EUR) -> kopa: "+(quantity*price)+" EUR");
+                    int id = Integer.valueOf(parts[0]);
+                    String name = parts[1];
+                    String product = parts[2];
+                    int quantity = Integer.valueOf(parts[3]);
+                    double price = Double.valueOf(parts[4]);
+                    sum += quantity * price;
+                    System.out.println("Pasutijums #"+id+": "+name+" pasutija "+quantity+" x "+product+" ("+price+" EUR) -> kopa: "+(quantity*price)+" EUR");
+                }
+                System.out.println("\nViss kopā: "+sum+" EUR");
             }
-        }
-        catch(FileNotFoundException e) {
-            System.out.println("Error: " + e.getMessage());
+            catch(FileNotFoundException e) {
+                System.out.println("Error: " + e.getMessage());
         }
     }
+}
