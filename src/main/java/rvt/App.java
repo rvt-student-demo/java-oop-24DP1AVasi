aaa7cb5cf553a5afaa862bf769af594b653eea3f
24DP1AVasi <a240731av@rvt.lv>
2026-01-06 16:29:44 +0200

    darbs

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,20 @@
 package rvt;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        try(Scanner scan = new Scanner(new File("data/data.csv"))) {
+            scan.nextLine(); // skip header
+            while(scan.hasNextLine()) {
+                System.out.println(scan.nextLine());
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
---
+++ src/main/java/rvt/PasutijumuVesture.java
@@ -0,0 +1,26 @@
+package rvt;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PasutijumuVesture {
+    try(Scanner scan = new Scanner(new File("data/data.csv"))) {
+            scan.nextLine(); // skip header
+            while(scan.hasNextLine()) {
+                String row = scan.nextLine();
+                String[] parts = row.split(",");
+
+                int id = Integer.valueOf(parts[0]);
+                String name = parts[1];
+                String product = parts[2];
+                int quantity = Integer.valueOf(parts[3]);
+                double price = Double.valueOf(parts[4]);
+
+                System.out.println("Pasutijums #"+id+": "+name+" pasutija "+quantity+" x "+product+" ("+price+" EUR) -> kopa: "+(quantity*price)+" EUR");
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
