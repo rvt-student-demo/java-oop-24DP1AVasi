32610063e9992c91c7dfa146993635119a98ba02
24DP1AVasi <a240731av@rvt.lv>
2026-01-20 16:22:41 +0200

    Stundas Darbs

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,16 +1,46 @@
 package rvt;
 
 
+import java.io.IOException;
+import java.io.FileWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        TodoList list = new TodoList();
-        Scanner scanner = new Scanner(System.in);
+        int taskNum = 1;
+        boolean stop = false;
+        Scanner scan = new Scanner(System.in);
+        try(FileWriter writer = new FileWriter("data/TodoList.csv", true)) {
+            writer.append("id,task\n");
+            while(stop == false) {
+                System.out.println("Comand: ");
+                String command = scan.nextLine();
 
-        UserInterface ui = new UserInterface(list, scanner);
-        ui.start();
+                if (command == "add") {
+                    System.out.print("Task: ");
+                    writer.append(taskNum +","+ scan.nextLine());
+                    taskNum ++;
+                    System.out.println();
+                }
+
+                if (command == "list") {
+
+                }
+
+                if (command == "completed") {
+
+                }
+
+                if (command == "stop") {
+                    stop = true;
+                }
+            }
+
+        } catch (IOException  e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
     }
 }
