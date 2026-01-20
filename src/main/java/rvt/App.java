d24d5b809352bd0d4986c00940007fb540059d22
Armands Andrejs Vasiļjevs <vasiljevs.armands@gmail.com>
2026-01-20 20:05:01 +0200

    Home

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -9,38 +9,55 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        try (FileWriter clean = new FileWriter("data/TodoList.csv", false)) {
+            clean.write("");
+        } catch (IOException e) {
+            System.err.println("Error clearing CSV file: " + e.getMessage());
+        }
         int taskNum = 1;
         boolean stop = false;
         Scanner scan = new Scanner(System.in);
         try(FileWriter writer = new FileWriter("data/TodoList.csv", true)) {
             writer.append("id,task\n");
             while(stop == false) {
-                System.out.println("Comand: ");
-                String command = scan.nextLine();
+                Scanner scanCsv = new Scanner(new File("data/TodoList.csv"));
+                System.out.print("Comand: ");
+                String command = scan.nextLine().toLowerCase();
 
-                if (command == "add") {
+                if (command.equals("add")) {
                     System.out.print("Task: ");
-                    writer.append(taskNum +","+ scan.nextLine());
+                    writer.append(taskNum + "," + scan.nextLine() + "\n");
                     taskNum ++;
-                    System.out.println();
                 }
 
-                if (command == "list") {
+                if (command.equals("list")) {
+                    scanCsv.nextLine();
+                    while (scanCsv.hasNextLine()) {
+                        String parts[] = scanCsv.nextLine().split(",");
+                        System.out.println(parts[0] + ": " + parts[1]);
+                    }
+                }
+
+                if (command.equals("remove")) {
+                    System.out.println("Which one you want to remove? ");
+                    int num = Integer.valueOf(scan.nextLine());
+                    scanCsv.nextLine();
+                    for (int i = 0; i < num; i++) {
+                        
+                    taskNum --;
+                    }
 
                 }
 
-                if (command == "completed") {
-
-                }
-
-                if (command == "stop") {
+                if (command.equals("stop")) {
                     stop = true;
+                    scanCsv.close();
+                    scan.close();
+                    
                 }
             }
-
         } catch (IOException  e) {
             System.out.println("Error: " + e.getMessage());
         }
-        
     }
 }
