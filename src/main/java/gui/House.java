72b56f171ed46fee62c716fec037d1ed5852df98
24DP1AVasi <vasiljevs.armands@gmail.com>
2026-05-04 16:07:35 +0300

    Completed the house drawing in gui

--- src/main/java/gui/House.java
+++ src/main/java/gui/House.java
@@ -8,9 +8,20 @@ public class House extends JPanel{
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		// Frame of the house
 		g.drawRect(400, 200, 200, 200);
 		g.drawLine(400,200,500, 100);
 		g.drawLine(500,100,600, 200);
+		// Attic window
 		g.drawOval(475, 135, 50, 50);
+		g.drawLine(500,135,500,185);
+		g.drawLine(475,160,525,160);
+		// Door
+		g.drawRect(430,290,50,110);
+		g.drawOval(465, 340, 10, 10);
+		// Groundfloor window
+		g.drawRect(520,290,50,50);
+		g.drawLine(545,290,545,340);
+		g.drawLine(520,315,570,315);
 	}
 }
--- src/main/java/gui/MyGui.java
+++ src/main/java/gui/MyGui.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class MyGui{
     public static void main(String[] args){
-        JFrame frame = new JFrame("Happy Coding");
+        JFrame frame = new JFrame("House");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         House house = new House();
