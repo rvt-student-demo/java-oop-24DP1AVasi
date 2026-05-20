d72fbd2d5fde6cd0258f917e6424e4399e57a938
24DP1AVasi <a240731av@rvt.lv>
2026-05-20 14:33:57 +0300

    java db izveidots

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,13 +1,23 @@
 package rvt;
 
-
-import java.io.IOException;
-import java.io.FileWriter;
-import java.io.File;
-import java.util.Scanner;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) {
-        
+        final String url = "jdbc:sqlite:todo.db";
+        try (
+            Connection conn = DriverManager.getConnection(url);        
+            Statement stmt = conn.createStatement();
+        ) {
+            String sql = "CREATE TABLE IF NOT EXISTS todo ("
+                + "id INTEGER PRIMARY KEY,"
+                + "task TEXT NOT NULL) STRICT";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
